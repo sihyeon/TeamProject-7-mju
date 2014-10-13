16a2844ef70e031eb5805e4be2d6a9aab2db00f0
Jaeung <darker826@gmail.com>
2014-10-13 23:06:01 +0900

    체크박스, 향수 목록부분 수정
    체크박스, 향수 목록부분 수정

--- PerfumeFinder/src/constants/Perfume_Info/Info_write.java
+++ PerfumeFinder/src/constants/Perfume_Info/Info_write.java
@@ -7,11 +7,20 @@ public class Info_write {
 	public void info(){ //향수 정보
 	PerfumeList = new Perfume_info_patten[Perfume_info_patten.Perfume_Kind]; 
 	
-	PerfumeList[0] = new Perfume_info_patten("로즈마리", 3000, new String[]{"탑"}, 
-								new String[]{ "미들"}, new String[]{"베이스"},  "브랜드", "이미지.jpg", "남"); //로즈마리향수
-	PerfumeList[1] = new Perfume_info_patten("라벤더", 3000, new String[]{"탑"}, 
-								new String[]{"미들"}, new String[]{"베이스"},  "브랜드", "이미지.png", "여"); //라벤더향수
-	}
+	PerfumeList[0] = new Perfume_info_patten("불가리 옴니아 아메시스트", 55000, new String[]{"홍자몽", "오렌지향"}, 
+								new String[]{ "불가리아 로즈버드", "아이리스", "장미"}, 
+								new String[]{"우디", "머스크향"},  "불가리", "불가리 옴니아 아메시스트.jpg", "여");
+	
+	PerfumeList[1] = new Perfume_info_patten("불가리 뿌르팜므 오데 퍼퓸", 55000, new String[]{"후레쉬 시트러스", "이탈리안 베르가못", "일랑일랑", "부케향"}, 
+								new String[]{"플로랄", "리빙 미모사", "리빙 프리뤼드로즈", "리빙 삼박 자스민"},
+								new String[]{"파우더리 무스크", "플로렌틴 아이리스", "무스크"},  "불가리", "불가리 뿌르팜므 오데 퍼퓸.jpg", "여"); 
+	
+	PerfumeList[2] = new Perfume_info_patten("불가리 오떼블랑 오데코롱", 52000, new String[]{"에이벌모스크", "아르테미시아 잎"}, 
+			new String[]{"화이트 페퍼"}, new String[]{"없음"},  "불가리", "불가리 오떼블랑 오데코롱.jpg", "여");
+	
+	PerfumeList[3] = new Perfume_info_patten("나르시소 포 힘 오드퍼퓸", 100000, new String[]{"아이리스", "핑크베리"}, 
+			new String[]{"엠버 그리스"}, new String[]{"파촐리"},  "나르시소 로드리게즈", "나르시소 포 힘 오드퍼퓸.jpg", "남");
+		}
 	
 }
 
