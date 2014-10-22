9c924351eb08070707b9b71ccd006f300f93e2a8
Jaeung <darker826@gmail.com>
2014-10-22 17:05:50 +0900

    다른 머지 만들기전에 마지막 커밋

--- PerfumeFinder/src/Frame/PFCheckBoxPanel.java
+++ PerfumeFinder/src/Frame/PFCheckBoxPanel.java
@@ -3,41 +3,47 @@ package Frame;
 import javax.swing.JPanel;
 import javax.swing.JCheckBox;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 import constants.PFConstants;
 
 
 public class PFCheckBoxPanel extends JPanel {
 	private EventHandler eHandler;
-	private PFCheckBoxPanel checkbox;
+	private JCheckBox check;
+	private static List<String> oPerlishArray = new ArrayList<String>();
 	public PFCheckBoxPanel(){
 		for(PFConstants.PFCheckBox checkbox : PFConstants.PFCheckBox.values()){	//Ã¼Å©¹Ú½º Ãß°¡
-			JCheckBox check = new JCheckBox(checkbox.toString());
+			check = new JCheckBox(checkbox.toString());
 			this.add(check);
 			eHandler = new EventHandler();
 			check.addActionListener(eHandler);
 		}
-	
 	}
 	private class EventHandler implements ActionListener{
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			JCheckBox  check = (JCheckBox)e.getSource();
-			if (check.isSelected()){
+			if (check.isSelected()){			
 				System.out.println(check.getText());
+				PFCheckBoxPanel.oPerlishArray.add(check.getText());
+				
 			}
 		}
 	}
-	public EventHandler geteHandler() {
-		return eHandler;
+
+	public List<String> getoPerlishArray() {
+		return oPerlishArray;
+	}
+
+	public void test(){
+		System.out.println("Å©ÇïÇï"+check.getText());
+		if (check.isSelected()){
+			System.out.println("Å©ÇïÇï"+check.getText());
+		}
 	}
 	
 }
--- PerfumeFinder/src/Frame/PFSearchButton.java
+++ PerfumeFinder/src/Frame/PFSearchButton.java
@@ -5,25 +5,31 @@ import javax.swing.ImageIcon;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import constants.PFConstants;
 
 public class PFSearchButton extends JButton {
 	private EventHandler eHandler;
 	private JButton btn;
-
+	PFCheckBoxPanel checkbox;
+	
 	public PFSearchButton(){
 		btn = new JButton(new ImageIcon(PFConstants.IMG_URL+"search.png"));
 		add(btn);
+		checkbox = new PFCheckBoxPanel();
 		eHandler = new EventHandler();
 		btn.addActionListener(eHandler);
 	}
 	private class EventHandler implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			JButton bt = (JButton)e.getSource();
+			checkbox.test();
+	//		 for (int i = 0; i < checkbox.getoPerlishArray().size(); i++){
+	//		      System.out.println(checkbox.getoPerlishArray().get(i));
+	//		 }
 			System.out.println("검색버튼 눌렀음");
-	//		checkbox = new PFCheckBoxPanel();
 		}
 	}
 	
